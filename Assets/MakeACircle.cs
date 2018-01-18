c3516ade7a6e9bb0907a61b260f6b0529d5ef121
mrome007 <mrome007@ucr.edu>
2018-01-17 22:24:37 -0800

    Start VR Hex Taisen over and prototype reticle indicator

---
+++ Assets/MakeACircle.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MakeACircle : MonoBehaviour 
+{
+    [SerializeField]
+    private LineRenderer circle;
+
+    [SerializeField]
+    private float radius;
+
+    private float timer = 5f;
+    private float theta;
+    private Vector3[] points;
+    private float increment;
+    private int index;
+    private void Start()
+    {
+        theta = 0f;
+        points = new Vector3[120];
+        increment = (2f * Mathf.PI) / timer;
+        index = 0;
+    }
+
+    private void Update()
+    {
+        MakeCircle();
+    }
+
+    private void MakeCircle()
+    {
+        if(index < points.Length)
+        {
+            theta += increment * Time.deltaTime;
+
+            var x = radius * Mathf.Cos(theta);
+            var y = radius * Mathf.Sin(theta);
+            index++;
+            circle.positionCount = index;
+            circle.SetPosition(index - 1, new Vector3(x, y, 0f));
+        }
+    }
+}
--- Assets/Scripts/HexGrid.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class HexGrid : MonoBehaviour
-{
-	public HexGrid[] AdjacentHexGrids;
-	public Unit UnitOccupying;
-}
