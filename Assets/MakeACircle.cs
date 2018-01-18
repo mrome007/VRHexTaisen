56c5f5cfeacfbc61ea6e4ccfc3007e6ca3be9325
mrome007 <mrome007@ucr.edu>
2018-01-17 22:56:43 -0800

    Use canvas instead of line renderer to make a radial progress indicator

--- Assets/MakeACircle.cs
+++ Assets/MakeACircle.cs
@@ -1,59 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MakeACircle : MonoBehaviour 
 {
     [SerializeField]
-    private LineRenderer circle;
+    private Image radialIndicator;
 
-    [SerializeField]
-    private float radius;
+    private float timer = 0f;
+    private float progressTime = 1f;
 
-    private float timer = 1.5f;
-    private float theta;
-    private List<Vector3> points;
-    private float increment;
-
-    private void Start()
-    {
-        theta = 0f;
-        points = new List<Vector3>();
-        increment = (2f * Mathf.PI) / timer;
-    }
 
     private void Update()
     {
         if(Input.GetKey(KeyCode.Space))
         {
-            MakeCircle();
+            FillUpIndicator();
         }
         else if(Input.GetKeyUp(KeyCode.Space))
         {
-            theta = 0f;
-            circle.positionCount = 0;
-            points.Clear();
+            timer = 0f;
+            radialIndicator.fillAmount = 0f;
         }
     }
 
-    private void MakeCircle()
+    private void FillUpIndicator()
     {
-        if(theta < 2f * Mathf.PI)
+        if(timer < progressTime)
         {
-            var x = radius * Mathf.Cos(theta);
-            var y = radius * Mathf.Sin(theta);
-            points.Add(new Vector3(x, y, 0f));
-            circle.positionCount = points.Count;
+            timer += Time.deltaTime;
 
-            theta += increment * Time.deltaTime;
-
-            if(theta >= 2f * Mathf.PI)
-            {
-                points.Add(new Vector3(radius, 0.05f, 0f));
-                circle.positionCount = points.Count;
-            }
-
-            circle.SetPositions(points.ToArray());
+            var fillPercentage = timer / progressTime;
+            radialIndicator.fillAmount = fillPercentage;
         }
     }
 }
