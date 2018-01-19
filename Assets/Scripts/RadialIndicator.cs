67df7bbda4cf24f7f591c1f3e30528aba8abae10
mrome007 <mrome007@ucr.edu>
2018-01-18 20:19:12 -0800

    Set up interacting with ui elements.

---
+++ Assets/Scripts/RadialIndicator.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RadialIndicator : MonoBehaviour
+{
+    private static Image radialIndicator;
+
+    private static float timer = 0f;
+    private static float progressTime = 1f;
+
+    private void Awake()
+    {
+        radialIndicator = GetComponent<Image>();
+    }
+
+    public static bool FillUpIndicator(bool hover)
+    {
+        if(hover)
+        {
+            if(timer < progressTime)
+            {
+                timer += Time.deltaTime;
+
+                var fillPercentage = timer / progressTime;
+                radialIndicator.fillAmount = fillPercentage;
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        else
+        {
+            timer = 0f;
+            radialIndicator.fillAmount = 0f;
+            return false;
+        }
+    }
+}
---
+++ Assets/Scripts/TaisenInteractable.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public abstract class TaisenInteractable : MonoBehaviour, IGvrPointerHoverHandler, IPointerEnterHandler, IPointerExitHandler
+{
+    public event EventHandler Success;
+
+    public virtual void OnGvrPointerHover(PointerEventData eventData)
+    {
+        RadialIndicator.FillUpIndicator(true);
+    }
+    
+    public virtual void OnPointerEnter(PointerEventData eventData)
+    {
+        RadialIndicator.FillUpIndicator(true);
+    }
+    
+    public virtual void OnPointerExit(PointerEventData eventData)
+    {
+        RadialIndicator.FillUpIndicator(false);
+    }
+}
