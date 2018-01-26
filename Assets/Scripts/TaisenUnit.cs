2cde6b5042bc3f3c8e15db6d0e618e2586e7ee80
mrome007 <mrome007@ucr.edu>
2018-01-25 21:40:19 -0800

    Fix player position when starting

--- Assets/Scripts/TaisenUnit.cs
+++ Assets/Scripts/TaisenUnit.cs
@@ -9,6 +9,6 @@ public class TaisenUnit : MonoBehaviour
     public void SetOccupiedTile(HexTile tile)
     {
         OccupiedTile = tile;
-        transform.position = new Vector3(tile.transform.position.x, 1.65f, tile.transform.position.z);
+        transform.position = new Vector3(tile.transform.position.x, transform.position.y, tile.transform.position.z);
     }
 }
